c16a74a8fa14444184826abc1b1b93a0ba0b8427
Yuri <yurijacksonalves@gmail.com>
2022-01-18 10:50:09 -0300

    adicionando o middleware

--- app/main.py
+++ app/main.py
@@ -2,12 +2,25 @@ from fastapi import FastAPI
 from .routers import ordemServico, user, auth
 from . import models
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 
+#models.Base.metadata.create_all(bind=engine) nao é mais usado
 
-#models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(ordemServico.rounter)
 app.include_router(user.router)
