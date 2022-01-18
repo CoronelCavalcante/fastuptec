621e5a3fdeadfbd5c09ae1f71740e94617874ab7
Yuri <yurijacksonalves@gmail.com>
2022-01-18 10:33:50 -0300

    adicionei o commando pra criar o primeiro usuario

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import models,schemas,utils, oauth2
 from sqlalchemy.orm import Session, query
@@ -38,3 +39,19 @@ def get_user(id: int, db: Session = Depends(get_db) ):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id: {id} was not found")
     
     return user
+
+
+@router.post("/adm", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_first_user(db: Session = Depends(get_db)):
+    hashed_password=utils.hash("adm123")
+    
+    new_user = models.Employee( email=  "adm@gmail.com", password = hashed_password, manager = True)
+    db.add(new_user)
+    try:
+        db.commit()
+        db.refresh(new_user)
+        return new_user
+    except IntegrityError:
+                db.rollback()
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Email ja registrado")
+
