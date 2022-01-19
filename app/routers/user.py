1dcb25a7f4ffebb8e1b3ae8694952d7c1d594837
Yuri <yurijacksonalves@gmail.com>
2022-01-19 09:51:13 -0300

    adicionei deletar usuario removi o adicionar adm

--- app/routers/user.py
+++ app/routers/user.py
@@ -55,3 +55,22 @@ def create_first_user(db: Session = Depends(get_db)):
                 db.rollback()
                 raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Email ja registrado")
 
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    #cursor.execute("""DELETE FROM posts WHERE id= %s RETURNING *""", (str(id),))
+   # deleted_post = cursor.fetchone()
+   # conn.commit()
+    if current_user.manager == False:        
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'Você não é autorizado a criar novos usuarios')
+    
+    emp_query = db.query(models.Employee).filter(models.Employee.id == id)
+    empDel = emp_query.first()
+    if empDel == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Employee with id: {id} does not exist')
+    if empDel.id == current_user.id:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Not Authorized to perform request action")
+    emp_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
